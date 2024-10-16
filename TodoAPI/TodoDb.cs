67477ae6e3ebaaf82b61698943015ff13555746d
michaelwestblade <michael.westblade@henrymeds.com>
2024-10-15 20:51:19 -0500

    basic todo api with in memory db

---
+++ TodoAPI/TodoDb.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI;
+
+public class TodoDb: DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options): base(options) {}
+    
+    public DbSet<TodoItem> Todos { get; set; }
+}
