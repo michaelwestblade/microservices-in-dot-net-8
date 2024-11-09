b5fe80171417cac7179f0a466dd6ef4c1bf9abf7
michaelwestblade <michael.westblade@henrymeds.com>
2024-11-09 13:39:08 -0600

    adding mediatr

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,16 @@
+using MediatR;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-public class CreateProductHandler
+internal class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, CreateProductResult>
 {
-    
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // business logic to create a product
+        throw new NotImplementedException();
+    }
 }
