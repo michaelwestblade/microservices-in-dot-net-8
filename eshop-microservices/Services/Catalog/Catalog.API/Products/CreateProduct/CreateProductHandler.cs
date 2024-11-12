c1ce9aba180bedc25ce3b1e2737120405b91b570
michaelwestblade <michael.westblade@henrymeds.com>
2024-11-11 20:14:43 -0600

    implemented interfaces in CreateProductHandler.cs

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,31 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // business logic to create a product
-        throw new NotImplementedException();
+        // create product entity from command object
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            CategoryIds = command.Category,
+            Description = command.Description,
+            ImageFileName = command.ImageFile,
+            Price = command.Price
+        };
+
+        // save to db
+
+        // return CreateProductResult result
+        return new CreateProductResult(product.Id);
     }
 }
