21e170a9bf59aeee25572254ea872abcfbae85b0
michaelwestblade <michael.westblade@henrymeds.com>
2024-11-11 20:32:07 -0600

    added GlobalUsing.cs

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,3 @@
-using Carter;
-using Mapster;
-using MediatR;
-
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
