5c9839ac25ff5186622d9acbac535f51d084e5a7
michaelwestblade <michael.westblade@henrymeds.com>
2024-11-14 08:40:12 -0600

    adding marten for db interaction

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,6 @@
 using BuildingBlocks.CQRS;
 using Catalog.API.Models;
+using Marten;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
@@ -8,14 +9,13 @@ public record CreateProductCommand(string Name, List<string> Category, string De
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session): ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         // create product entity from command object
         var product = new Product
         {
-            Id = Guid.NewGuid(),
             Name = command.Name,
             CategoryIds = command.Category,
             Description = command.Description,
@@ -24,6 +24,8 @@ internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand
         };
 
         // save to db
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // return CreateProductResult result
         return new CreateProductResult(product.Id);
