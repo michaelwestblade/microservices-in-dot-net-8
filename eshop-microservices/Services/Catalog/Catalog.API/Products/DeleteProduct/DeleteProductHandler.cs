d98c0748bfe94b8039525ceea7371f88f35411fa
michaelwestblade <michael.westblade@henrymeds.com>
2024-11-26 11:10:11 -0600

    added delete command and endpoint

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,30 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger): ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"DeleteProductCommandHandler.Handle called with command {command}");
+        
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+        
+        session.Delete(product);
+        await session.SaveChangesAsync(cancellationToken);
+        
+        return new DeleteProductResult(true);
+    }
+}
