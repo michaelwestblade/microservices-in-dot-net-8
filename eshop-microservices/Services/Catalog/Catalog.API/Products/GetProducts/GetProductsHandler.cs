1903c82b6c05cd8c33c63df362ceda69a455bcf0
michaelwestblade <michael.westblade@henrymeds.com>
2024-12-30 15:05:17 -0600

    added pagination to get products

--- eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,10 +1,11 @@
 using BuildingBlocks.CQRS;
 using Catalog.API.Models;
 using Marten;
+using Marten.Pagination;
 
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<Product> Products);
 
@@ -12,7 +13,7 @@ internal class GetProductsQueryHandler(IDocumentSession session): IQueryHandler<
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync();
+        var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
         
         return new GetProductsResult(products);
     }
