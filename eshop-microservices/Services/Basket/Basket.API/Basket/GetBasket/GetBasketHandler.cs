a6c4043ccd09ad50b1205f928a87a0d7c950b2be
michaelwestblade <michael.westblade@henrymeds.com>
2024-12-31 10:12:18 -0600

    get basket handler

--- eshop-microservices/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ eshop-microservices/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,19 @@
+using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
 namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler: IQueryHandler<GetBasketQuery, GetBasketResult>
 {
-    
+    public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+    {
+        // TODO: get basket from db
+        //var basket = await _repository.GetBasket(request.UserName)
+        
+        return new GetBasketResult(new ShoppingCart("SWN"));
+    }
 }
