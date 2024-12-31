684a74650f8e26bd280cf49df0fb7fa73d7aebc6
michaelwestblade <michael.westblade@henrymeds.com>
2024-12-31 10:32:44 -0600

    store basket endpoints

--- eshop-microservices/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ eshop-microservices/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,6 +1,30 @@
+using Basket.API.Models;
+using Carter;
+using Mapster;
+using MediatR;
+
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndpoints
+public record StoreBasketRequest(ShoppingCart Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoints: ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<StoreBasketResponse>();
+                
+                return Results.Created($"/basket/{response.UserName}", response);
+        })
+            .WithName("CreateBasket")
+            .Produces(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create a basket")
+            .WithDescription("Create a basket");
+    }
 }
