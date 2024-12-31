55223d3bae08c9c7f171baa47fb3659020c70c92
michaelwestblade <michael.westblade@henrymeds.com>
2024-12-31 10:32:44 -0600

    delete basket handler

--- eshop-microservices/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ eshop-microservices/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,29 @@
+using Basket.API.Basket.StoreBasket;
+using Basket.API.Models;
+using BuildingBlocks.CQRS;
+using FluentValidation;
+
 namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommad(string UserName): ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommad>
 {
-    
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotNull().NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketHandler: ICommandHandler<DeleteBasketCommad, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommad command, CancellationToken cancellationToken)
+    {
+        // TODO delete basket in db
+        // TODO update cache
+        
+        return new DeleteBasketResult(true);
+    }
 }
