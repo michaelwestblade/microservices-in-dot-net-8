8ed973457ace2d33731f758d0439bacf30f2ebd4
michaelwestblade <michael.westblade@henrymeds.com>
2024-12-31 10:40:04 -0600

    delete basket endpoints

--- eshop-microservices/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ eshop-microservices/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -5,11 +5,11 @@ using FluentValidation;
 
 namespace Basket.API.Basket.DeleteBasket;
 
-public record DeleteBasketCommad(string UserName): ICommand<DeleteBasketResult>;
+public record DeleteBasketCommand(string UserName): ICommand<DeleteBasketResult>;
 
 public record DeleteBasketResult(bool IsSuccess);
 
-public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommad>
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
     public DeleteBasketCommandValidator()
     {
@@ -17,9 +17,9 @@ public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommad
     }
 }
 
-public class DeleteBasketHandler: ICommandHandler<DeleteBasketCommad, DeleteBasketResult>
+public class DeleteBasketHandler: ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
 {
-    public async Task<DeleteBasketResult> Handle(DeleteBasketCommad command, CancellationToken cancellationToken)
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
     {
         // TODO delete basket in db
         // TODO update cache
