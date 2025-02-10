a0bc0fd491bf041ae3d811d5f805d3ef9de5b7c6
= <=>
2025-02-10 09:15:58 -0600

    setting up DependencyInjection.cs

---
+++ eshop-microservices/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,14 @@
+namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAPIServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static WebApplication UseAPIServices(this WebApplication app)
+    {
+        return app;
+    }
+}
---
+++ eshop-microservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+        
+        // services.AddDBContext<ApplicationDBContext>(options =>
+        // {
+        //     options.UseSqlServer(connectionString)
+        // });
+        
+        return services;
+    }
+}
