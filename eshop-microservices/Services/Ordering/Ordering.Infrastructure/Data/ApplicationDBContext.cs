781f6e075719b0ba1c7c4832a37aaec18e7bda9b
= <=>
2025-03-03 08:58:57 -0600

    adding app db context

---
+++ eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using Ordering.Domain.Models.ValueObjects;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDBContext: DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+    {
+        
+    }
+    
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
